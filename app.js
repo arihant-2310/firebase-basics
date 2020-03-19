c7acc28f566f4437acca287d338a47632f75ebc4
arihant-2310 <36256102+arihant-2310@users.noreply.github.com>
2020-03-19 15:17:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ window.alert('Time to hit the boredom, lets chat!!');
 const chatList = document.querySelector('#chat-list');
 const form = document.querySelector('#add-chat-form');
 
+
 //create element and render cafe
 function renderCafe(doc){
     let li = document.createElement('li');
@@ -42,7 +43,8 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault();
     db.collection('cafes').add({
         name: form.name.value,
-        message:form.message.value
+        message:form.message.value,
+        timestamp:firebase.firestore.FieldValue.serverTimestamp()
     });
     form.name.value='';
     form.message.value= '';
@@ -50,7 +52,7 @@ form.addEventListener('submit',(e)=>{
 
 
 //real time data
-db.collection('cafes').orderBy('name').onSnapshot(snapshot =>{
+db.collection('cafes').orderBy('timestamp','asc').onSnapshot(snapshot =>{
     let changes = snapshot.docChanges();
     changes.forEach(change=>{
         if(change.type=='added'){
