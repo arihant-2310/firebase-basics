f5e68c7ecc1128adbe414df78cb7b3b03fe81651
arihant-2310 <arihant2310@gmail.com>
2020-03-19 14:26:40 +0530

    first

---
+++ app.js
@@ -0,0 +1,64 @@
+window.alert('Time to hit the boredom, lets chat!!');
+const chatList = document.querySelector('#chat-list');
+const form = document.querySelector('#add-chat-form');
+
+//create element and render cafe
+function renderCafe(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let message = document.createElement('span');
+    let cross = document.createElement('div');
+    
+    li.setAttribute('data-id',doc.id);
+
+    name.textContent = doc.data().name;
+    message.textContent = doc.data().message;
+    cross.textContent = 'x';
+
+
+    li.appendChild(name);
+    li.appendChild(message);
+    li.appendChild(cross);
+   
+
+
+    chatList.appendChild(li);
+
+    //deleting data
+    cross.addEventListener('click', (e)=>{
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
+}
+// //getting data
+// db.collection('cafes').get().then((snapshot)=>{
+//     snapshot.docs.forEach(doc =>{
+//         renderCafe(doc);
+//     })
+// })
+
+//saving data
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        message:form.message.value
+    });
+    form.name.value='';
+    form.message.value= '';
+})
+
+
+//real time data
+db.collection('cafes').orderBy('name').onSnapshot(snapshot =>{
+    let changes = snapshot.docChanges();
+    changes.forEach(change=>{
+        if(change.type=='added'){
+            renderCafe(change.doc);
+        }
+        else if(change.type=='removed'){
+            let li = chatList.querySelector('[data-id='+ change.doc.id + ']');
+            chatList.removeChild(li);
+        }
+    })
+})
